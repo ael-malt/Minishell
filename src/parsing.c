cd426151e9420f7083cae9156dd99097e5980bd1
Lara ZANETTI <lazanett@paul-f4Ar2s4.clusters.42paris.fr>
2023-09-21 15:26:06 +0200

    tstrrchr

--- src/parsing.c
+++ src/parsing.c
@@ -6,27 +6,27 @@
 /*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:12:43 by lazanett          #+#    #+#             */
-/*   Updated: 2023/09/21 14:37:33 by lazanett         ###   ########.fr       */
+/*   Updated: 2023/09/21 15:25:06 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	ft_strrchr_(const char *s, char c)
-{
-	int	i;
+// int	ft_strrchr_(const char *s, char c)
+// {
+// 	int	i;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	while (i >= 0)
-	{
-		if (s[i] == c)
-			return (1);
-		i--;
-	}
-	return (0);
-}
+// 	i = 0;
+// 	while (s[i] != '\0')
+// 		i++;
+// 	while (i >= 0)
+// 	{
+// 		if (s[i] == c)
+// 			return (1);
+// 		i--;
+// 	}
+// 	return (0);
+// }
 
 int	search_char(char *s) // chercher ';' et '\'
 {
@@ -35,7 +35,7 @@ int	search_char(char *s) // chercher ';' et '\'
 	i = 0;
 	while (s[i])
 	{
-		if (ft_strrchr_(&s[i], '\\') == 1 || ft_strrchr_(&s[i], ';') == 1)
+		if (s[i] == '\\'1 || s[i], ';')
 			return (1);
 		i++;
 	}
