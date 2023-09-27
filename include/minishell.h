0df678876008ff36083f96fbf138df569b588284
Lara Zanett <larazanetti207@gmail.com>
2023-09-27 17:06:37 +0200

    tree_b

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:10:09 by lazanett          #+#    #+#             */
-/*   Updated: 2023/09/22 15:36:29 by lazanett         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:34:34 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,26 +22,54 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+// enum operator
+// {
+// 	PIPE, // |
+// 	DREDIRIN, //heredoc
+// 	DREDIROUT, // ajout a la fin d'un file
+// 	REDIRIN, // <
+// 	REDIROUT, // >
+// };
+// https://learn.microsoft.com/fr-fr/cpp/c-language/union-declarations?view=msvc-170
+// typedef struct s_type
+// {
+// 	s_type	operator;
+// 	s_type	command;
+// }	t_type
+
 typedef struct s_tree
 {
 	char			*content;
+	char	*str1;
+	char	*str2;
+	int		len_command;
+	int		len_str1;
 	struct s_tree	*left;
 	struct s_tree	*right;
 }	t_tree;
 
 //------------------------------PARSING.C------------------------------//
-//int	ft_strrchr_(const char *s, char c);
 int	search_char(char *s);
 int	search_quote(char *s);
-int	limiteur(char c);
 //---------------------------------------------------------------------//
 
 //----------------------------MAIN.C-----------------------------------//
 char	**get_tab_env(char **envp);
+t_tree	*create_node();
+void	len_split_command(t_tree *tree);
+void	split_command(t_tree *tree);
+char	*ft_strndup(char *s, int start, int end);
+void	is_operator_split(t_tree *tree);
+int	len_operator(t_tree *tree);
 //---------------------------------------------------------------------//
 
 //------------------------------TREE.C------------------------------//
-t_tree	create(char *content);
-t_tree *insert(t_tree *tree, char *content);
+
 //---------------------------------------------------------------------//
+
+//---------------------TAB_COMMANDE--------------------------------------//
+
+
+//---------------------------------------------------------------------//
+
 #endif
--- src/main.c
+++ src/main.c
@@ -6,34 +6,44 @@
 /*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:12:04 by lazanett          #+#    #+#             */
-/*   Updated: 2023/09/21 14:10:36 by lazanett         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:45:23 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main(int ac, char **av, char **envp)
-{
-	(void) av;
-	char	*line;
-	if (ac == 1)
-	{
-		get_tab_env(envp);
-		while (1)
-		{
-			line = readline("Minishell: ");
-			if (line)
-			{
-				add_history(line);
-				if (search_char(line) == 1 || search_quote(line) == 1)
-					printf("Error : line invalid \n");
-			}
-			//free line
-		}
-	}
-	else
-		printf("Error : nb argc invalid\n");
-}
+// int	main(int ac, char **av, char **envp)
+// {
+// 	(void) av;
+// 	char	*line;
+// 	if (ac == 1)
+// 	{
+// 		get_tab_env(envp);
+		
+// 		while (1)
+// 		{
+// 			line = readline("Minishell: ");
+// 			if (line)
+// 			{
+// 				add_history(line);
+// 				if (search_char(line) == 1 || search_quote(line) == 1)
+// 					printf("Error : line invalid \n");
+//				if (is_command_simple(line) == 0)
+//					get_str(line);
+//				else
+//				{
+// 					create_tree;
+//					get_str(line);
+//				}
+// 				
+// 			}
+// 			//free line
+// 		}
+// 	}
+// 	else
+// 		printf("Error : nb argc invalid\n");
+// }
+
 
 char	**get_tab_env(char **envp) // recup l'environnement
 {
@@ -56,5 +66,119 @@ char	**get_tab_env(char **envp) // recup l'environnement
 	return (tab);
 }
 
+void	split_command(t_tree *tree) // simple ppinteur car renvoie une string
+{
+	len_split_command(tree);
+	if (tree->len_command == tree->len_str1)// si le len = \0 quitte fonction
+		return;
+	if (tree->content[0] == '|')
+		is_operator_split(tree);
+	else
+	{
+		tree->str1 = ft_strndup(tree->content, 0, (tree->len_str1 - 1));
+		tree->str2 = ft_strndup(tree->content, tree->len_str1, (tree->len_command - 1));
+	}
+	if (tree->str1)
+	{
+		tree->left = create_node();
+		tree->left->content = tree->str1;
+		printf("tree->left = %s\n", tree->left->content);
+		tree->right = create_node();
+		tree->right->content = tree->str2;
+		printf("tree->right = %s\n", tree->right->content);
+		split_command(tree->right);
+	}
+	//flag = 1;
+	return;
+}
+// faire fonction pour detecter les operator
 
+void	is_operator_split(t_tree *tree)
+{
+	int	i;
 
+	i = 0; 
+	tree->str1 = malloc(sizeof (char) * (len_operator(tree) + 1));
+	while (tree->content[i] && tree->content[i] == '|')
+	{
+		tree->str1[i] = tree->content[i];
+		i++;
+	}
+	tree->str1[i] = '\0';
+	while (tree->content[i] == ' ')
+		i++;
+	tree->str2 = ft_strndup(tree->content, i, (tree->len_command - 1));
+}
+
+int	len_operator(t_tree *tree)
+{
+	int	i;
+
+	i = 0;
+	while (tree->content[i] && tree->content[i] == '|')
+		i++;
+	return (i);
+}
+
+void	len_split_command(t_tree *tree)
+{
+	int	i;
+
+	i = 0;
+	//printf("%s\n", tree->content);
+	while (tree->content[i] != '\0')
+		i++;
+	tree->len_command = i;
+	i = 0;
+	while (tree->content[i] != '\0' && tree->content[i] != '|')
+		i++;
+	tree->len_str1 = i;
+	//printf("len str1 = %d\n", tree->len_str1);
+	//printf("len command = %d\n", tree->len_command);
+}
+
+int	main(int ac, char **av)
+{
+	t_tree	*tree;
+	
+	if (ac == 2)
+	{
+		tree = create_node();
+		tree->content = av[1];
+		split_command(tree);
+	}
+}
+
+char	*ft_strndup(char *s, int start, int end)
+{
+	int i;
+	char *copy;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if ((end - start) < 0)
+		return (NULL);
+	copy = malloc(sizeof (char) * (end - start) + 1);
+	if (!copy)
+		return (NULL);
+	while (s[start] && start <= end)
+	{
+		copy[i] = s[start];
+		start++;
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+t_tree	*create_node()
+{
+	t_tree	*node;
+
+	node = malloc(sizeof(t_tree)); 
+	if (!node)
+		return (NULL);
+	ft_memset(node, 0, sizeof(t_tree));
+	return (node);
+}
