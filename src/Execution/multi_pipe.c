14b68cb24514d5a5b7bf4a2692cd7fd595315b89
Lara ZANETTI <lazanett@student.42.fr>
2023-11-22 18:36:34 +0100

    rm_output

--- src/Execution/multi_pipe.c
+++ src/Execution/multi_pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   multi_pipe.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ael-malt <ael-malt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lazanett <lazanett@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 11:24:15 by lazanett          #+#    #+#             */
-/*   Updated: 2023/11/22 15:58:16 by ael-malt         ###   ########.fr       */
+/*   Updated: 2023/11/22 18:33:04 by lazanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,59 +18,6 @@ extern int	g_exit_status;
 // Ajouter < et << au redirections multipipes
 // Gerer les redirections en dernier maillon
 // Refaire mon heardoc
-
-// void	multi_pipe(t_lst *lst, t_expand *ex)
-// {
-// 	int	i;
-// 	int	nb_operator;
-// 	int	fd[2];
-// 	int	fd_temp;
-
-// 	i = 0;
-// 	fd_temp = 0;
-// 	nb_operator = lst_count_pipe(lst) + lst_count_redir(lst);
-	
-// 	while (i <= nb_operator)
-// 	{
-// 		// if (i < (nb_operator))
-// 		// {
-// 		// 	if (lst->next && lst->token != 2 && lst->next->token == 1)
-// 		// 	{
-// 		// 		pipex(fd, &fd_temp, lst, ex);
-// 		// 		if (lst->next != NULL && lst->next->next != NULL)
-// 		// 			lst = lst->next->next;
-// 		// 	}
-// 		// 	else if (lst->next && (lst->next->token == 2 || lst->token == 2))
-// 		// 	{
-// 		// 		redirex(&fd_temp, lst, ex);
-// 		// 		while (lst->next)
-// 		// 		{
-// 		// 			ft_printf("content: %s\n	", lst->content);
-// 		// 			lst = lst->next;
-// 		// 			if (lst->token == 0 || lst->token == 2)
-// 		// 				break ;
-// 		// 		}
-// 				// if (lst->next && lst->next->token == 0 && lst->next->next 
-// 				// 	&& lst->next->next->token == 1 && lst->next->next->next)
-// 				// 	lst = lst->next->next->next;
-// 				// else if (lst->next && lst->next->next && 
-// 				// (lst->next->token == 0 || lst->next->token == 1))
-// 				// 	lst = lst->next->next;
-// 				// else if (lst->next)
-// 				// 	lst = lst->next;
-// 			// }
-// 		}
-// 		else
-// 		{
-// 			if (get_last_operator(lst) == 1)
-// 				last_pipe(fd, &fd_temp, lst, ex);
-// 			// else if (get_last_operator(lst) == 2)
-// 			// 	last_redir(fd, &fd_temp, lst, ex);
-// 		}
-// 		i++;
-// 	}
-// 	wait(NULL);
-// }
 
 void	execute(t_lst *lst, t_expand *ex)
 {
@@ -85,11 +32,11 @@ void	execute(t_lst *lst, t_expand *ex)
 		excecuting(lst, ex->tab);
 }
 
-void redirect(t_lst *lst, int fd_temp) {	
+void redirect(t_lst *lst, int fd_temp)
+{	
 	int	file;
 
 	lst = lst->next;
-
 	file = open_redir_file(lst);
 	while (lst->next && (is_redir(lst) == is_redir(lst->next) || is_redir(lst) == (is_redir(lst->next) + 2) || is_redir(lst) == (is_redir(lst->next) - 2)))
 	{
@@ -97,7 +44,7 @@ void redirect(t_lst *lst, int fd_temp) {
 		lst = lst->next;
 		file = open_redir_file(lst);
 	}
-	printf("rex: %s %d %d\n", lst->command, lst->token, file);
+	// printf("rex: %s %d %d\n", lst->command, lst->token, file);
 	redirex(file, &fd_temp, lst);
 }
 
@@ -125,7 +72,6 @@ void	multi_pipe(t_lst *lst, t_expand *ex)
 			{
 				signal(SIGQUIT, SIG_DFL);
 				// fprintf(stderr, "cmd: %s token: %d next: %d next_token: %d\n", lst->command ? lst->command : "no", lst->token, lst->next ? 1 : 0, lst->next ? lst->next->token : -1);
-
 				if ((lst->prev && lst->prev->token == 1) || (lst->next && lst->next->token == 1))
 				{
 					// fprintf(stderr, "pipex\n");
@@ -136,7 +82,6 @@ void	multi_pipe(t_lst *lst, t_expand *ex)
 					// fprintf(stderr, "redirect\n");
 					redirect(lst, fd_temp);
 				}
-
 				// fprintf(stderr, "execute %s %d\n", lst->command, fd_temp);
 				execute(lst, ex);
 			}
@@ -172,11 +117,6 @@ void	redirex(int file, int *fd_temp, t_lst *lst)
 			mini_perror(PIPERR, NULL, 1);
 			return ;
 		}
-		/*if (dup2(file, STDOUT_FILENO) == -1)
-		{
-			mini_perror(PIPERR, NULL, 1);
-			return ;
-		}*/
 	}
 	else if (is_redir(lst) == 1 || is_redir(lst) == 3)
 	{
@@ -214,26 +154,3 @@ void	pipex(int *fd, int *fd_temp, t_lst *lst)
 	close(*fd_temp);
 	close(fd[1]);
 }
-
-
-// void	exc_redir(int *fd, int fd_temp, t_lst *lst, t_expand *ex)
-// {
-// 	signal(SIGQUIT, SIG_DFL);
-// 	if (dup2(fd_temp, STDIN_FILENO) == -1)
-// 		ft_perror("Dup");
-// 	close(fd[0]);
-// 	if (dup2(fd[1], STDOUT_FILENO) == -1)
-// 		perror("Dup");
-// 	close(fd_temp);
-// 	close(fd[1]);
-// 	if (is_builtin(lst))
-// 	{
-// 		builtin(lst, ex);
-// 		exit(0);
-// 	}
-// 	else if (ft_strchr(lst->split_command[0], '/') != NULL)
-// 		exc_absolut_way(lst, ex);
-// 	else
-// 		excecuting(lst, ex->tab);
-// }
-
